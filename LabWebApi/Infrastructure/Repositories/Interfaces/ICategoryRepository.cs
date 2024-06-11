912987adc9854f117f43a3c1b7bf25cd7b935d38
stanirl <stanislaw.skotniczny@microsoft.wsei.edu.pl>
2024-06-11 19:05:23 +0200

    Add project files.

---
+++ LabWebApi/Infrastructure/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using LabWebApi.Core.Entities;
+
+namespace LabWebApi.Infrastructure.Repositories.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> GetCategoryByIdAsync(int id);
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task AddCategoryAsync(Category category);
+        Task UpdateCategoryAsync(Category category);
+        Task DeleteCategoryAsync(int id);
+    }
+}
---
+++ LabWebApi/Infrastructure/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using LabWebApi.Core.Entities;
+
+namespace LabWebApi.Infrastructure.Repositories.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task<Order> GetOrderByIdAsync(int id);
+        Task<IEnumerable<Order>> GetOrdersAsync();
+        Task AddOrderAsync(Order order);
+        Task UpdateOrderAsync(Order order);
+        Task DeleteOrderAsync(int id);
+    }
+}
